bd3acc73c2105f5dbf01ea3872c2a268378bfb8f
kiranbhoi <kiran.bhoi@parallel-minds.com>
2023-06-26 11:39:15 +0530

    "Student models changes"

---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
